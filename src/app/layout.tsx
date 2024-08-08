151d3db280e5961d1fe8c3959f57c46be8dba82a
Carter Singletary <120229354+cartersingle@users.noreply.github.com>
2024-08-08 16:52:51 -0600

    add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
 import { type ReactNode } from "react";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Carter Singletary",
@@ -13,7 +18,14 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 };
